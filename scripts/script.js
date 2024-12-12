5217cd5e6757124a552456182449deeb3e881b4c
Anouarab2 <anouar.ait.brahim@hva.nl>
2024-12-12 23:40:18 +0100

    fix voor code review #4 #5 #6 #8 #9

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,31 @@
-// funtie aanrooepen
-function showSidebar(){
-// de class selecteren
-    const sidebar = document.querySelector('.sideBar')
-// aangeven dat hij laten zien moet worden
-    sidebar.style.display = 'flex'
+// OPENT EN SLUIT HET HAMBURGER MENU
+
+// selecteert de elementen die aangesproken moeten worden
+const hamMenu = document.querySelector('.ham-menu');
+const hamItems = document.querySelector('.ham-items');
+
+// voegt een functie toe die moet gebeuren bij het klikken
+hamMenu.addEventListener('click', openMenu)
+
+// vertelt wat de functie moet zijn (een class togglen)
+function openMenu(){
+    hamMenu.classList.toggle('active')
+    hamItems.classList.toggle('active')
 }
 
-// funtie aanroepen
-function closeSidebar(){
-// de class selecteren 
-    const sidebar = document.querySelector('.sideBar')
-// aangeven dat hij verstopt moet worden     
-    sidebar.style.display = 'none'
+
+// LAAT HARRY DRAAIEN
+
+// selecteert het element wat aangesproken moet worden
+let rotateSection = document.querySelector('.harry')
+
+// voegt een funnctie toe die moet gebeuren bij het klikken
+rotateSection.addEventListener('click', sectionRotate)
+
+// vertelt wat de functie moet zijn (harry laten draaien met keyframes)
+function sectionRotate() {
+  rotateSection.classList.toggle('rotate')
 }
+
+// zorgt dat de class die is toegevoegd eraf gaat als de animatie klaar is
+rotateSection.addEventListener('animationend', sectionRotate)
